3c75e08a69004aec1e0b6842ab5ca6839e4473e2
yangyang4cs <yangyang4j@gmail.com>
2014-03-20 22:43:09 -0700

    finished ep2 of tutorial

---
+++ src/com/youtube/rest/status/V1_status.java
@@ -0,0 +1,57 @@
+package com.youtube.rest.status;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * This is the root path for our restful api service In the web.xml file, we
+ * specified that /api/* need to be in the URL to get to this class.
+ * 
+ * We are versioning the class in the URL path. This is the first version v1.
+ * Example how to get to the root of this api resource:
+ * http://localhost:7001/com.youtube.rest/api/v1/status
+ * 
+ * 
+ * Readme: http://localhost:7001/com.youtube.rest/
+ * 
+ * API Title: http://localhost:7001/com.youtube.rest/api/v1/status
+ * 
+ * API Version: http://localhost:7001/com.youtube.rest/api/v1/status/version
+ * 
+ */
+@Path("v1/status/")
+public class V1_status {
+
+	private static final String api_version = "00.01.00"; // version of the api
+
+	/**
+	 * This method sits at the root of the api. It will return the name of this
+	 * api.
+	 * 
+	 * @return String - Title of the api
+	 */
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnTitle() {
+		return "<p>Java Web Service</p> ";
+	}
+
+	/**
+	 * This method will return the version number of the api Note: this is
+	 * nested one down from the root. You will need to add version intro the URL
+	 * Path.
+	 * 
+	 * Example: http://localhost:7001/com.youtube.rest/api/v1/status/version
+	 * 
+	 * @return String - version number of the api
+	 */
+	@Path("/version")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnVersion() {
+		return "<p>Version:</p> " + api_version;
+	}
+
+}
